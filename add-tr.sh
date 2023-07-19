65be8c56bc4a699a6789994d44cbf8b4c25e4bf0
ndmservice <139954588+ndmservice@users.noreply.github.com>
2023-07-19 15:41:44 +0700

    Update add-tr.sh

--- add-tr.sh
+++ add-tr.sh
@@ -40,7 +40,7 @@ export UNDERLINE="\e[4m"
 
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/wingsofhope123/permission/main/ip > /root/tmp
+    curl -sS https://raw.githubusercontent.com/ndmservice/permission/main/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -58,7 +58,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/wingsofhope123/permission/main/ip | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/ndmservice/permission/main/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -75,7 +75,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/wingsofhope123/permission/main/ip | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/ndmservice/permission/main/ip | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -151,7 +151,7 @@ echo -e "${BIBlue}════════════════════�
 echo -e "Link GRPC : "
 echo -e "${trojanlink1}"
 echo -e "${BIBlue}════════════════════════════════════${NC}" 
-echo -e "${BICyan} Wings Premium VPN Auto Script Service${NC}" 
+echo -e "${BICyan} NDM Premium VPN Auto Script Service${NC}" 
 echo "" 
 read -n 1 -s -r -p "Press any key to back on menu"
 
